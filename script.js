9025088e3c704b0610fcd3e7d8aa4a6f339cb621
Adão carvalho <104265224+acpcarvalhoh@users.noreply.github.com>
2024-06-12 15:15:42 -0300

    removendo ação do slide

--- script.js
+++ script.js
@@ -18,32 +18,4 @@ document.addEventListener('scroll', function() {
   }
 });
 
-// Função para adicionar classe "active" ao item da lista de navegação correspondente à seção atual
-function setActiveNavItem() {
-  // Obter todas as seções da página
-  const sections = document.querySelectorAll('section');
-  
-  // Iterar sobre as seções
-  sections.forEach(section => {
-      // Verificar se a seção está visível na tela
-      const rect = section.getBoundingClientRect();
-      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-          // Remover a classe "active" de todos os itens da lista de navegação
-          document.querySelectorAll('.splide__list li').forEach(item => {
-              item.classList.remove('active');
-          });
-          
-          // Obter o link correspondente à seção atual
-          const link = document.querySelector(`.splide__list a[href="#${section.id}"]`);
-          if (link) {
-              // Adicionar a classe "active" ao item da lista de navegação
-              link.parentNode.classList.add('active');
-          }
-      }
-  });
-}
-
-// Chamar a função quando a página for carregada e quando a janela for rolada
-window.addEventListener('DOMContentLoaded', setActiveNavItem);
-window.addEventListener('scroll', setActiveNavItem);
 
