5350fb4933666eb8ccf0178b121cb80fe22dd3c6
acp <carvalhohuzumak@gmail.com>
2024-06-13 09:51:45 -0300

    resolvido divergencias no leyout

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ items.forEach((item, index) => {
 document.addEventListener('scroll', function() {
   const header = document.querySelector('header');
   const nav = document.querySelector('nav');
-  const sec = document.querySelector('section');
+
 
   if (window.scrollY >= header.offsetHeight) {
     nav.classList.add('fixed');
-    sec.classList.add('ad-margin');
+   
   } else {
     nav.classList.remove('fixed');
-    sec.classList.remove('ad-margin');
+
   }
 });
 
